617ab62144caf8f7b9f0e063512bbee3af9df62a
Adrian Gamez <adrian.f.gamez@gmail.com>
2017-01-18 23:31:10 +0000

    First Try

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//Write your function calls below this line
+
+// Write your code below this line
+setColor(9,0,"purple");
+setColor(0,0,"purple");
+setColor(0,9,"purple");
+setColor(9,9,"purple");
+setColor(5,5,"red");
